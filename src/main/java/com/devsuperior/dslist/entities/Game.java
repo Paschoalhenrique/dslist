689bc270964372b1724cddfdcb15453dd6b5bd47
Paschoal Henrique <paschoalhenrique@hotmail.com>
2023-05-09 13:02:45 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,23 +21,29 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	//campo de caracteres maior que 256 essa anotação define que jpa vai definir como texto dentro do banco de dados H2
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	//campo de caracteres maior que 256 essa anotação define que jpa vai definir como texto dentro do banco de dados H2
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -76,12 +82,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platform) {
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {
