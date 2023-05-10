a00655a847dce31a1dc4231bc73ecf13c7b8b3fd
Paschoal Henrique <paschoalhenrique@hotmail.com>
2023-05-10 16:25:49 -0300

    Consulta SQL, projection, busca de games por lista

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection {
+
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String GetShortDescription();
+	Integer getPosition();
+	String getShortDescription();
+	
+}
